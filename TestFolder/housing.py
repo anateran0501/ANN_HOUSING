eb21e5cbeee2e4ec9ea2dc2a0299bcf284fb1a74
Ana T. <163786190+anateran0501@users.noreply.github.com>
2024-11-21 17:17:41 -0500

    Update and rename housing.py to TestFolder/housing.py
    Changing location, merely

--- TestFolder/housing.py
+++ TestFolder/housing.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+
 from sklearn.linear_model import LinearRegression, Ridge, Lasso, ElasticNet
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
